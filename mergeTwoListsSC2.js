a3b3a77452c3c8fdcc8f13c6852e49545d5f2c79
Royalthius <arathurs@gmail.com>
2020-11-14 19:03:06 -0500

    Completed writing solution in mergeTwoListsSC2.js before testing.

--- mergeTwoListsSC2.js
+++ mergeTwoListsSC2.js
@@ -4,17 +4,34 @@ var mergeTwoLists = function(l1, l2) {
         v = l2,
         curr = dummyHead;
     while (q !== null || v !== null) {
-        if (
-            (q && v && q.val !== null && v.val !== null && q.val <= v.val)
-         ||  q && q.val !== null && (!v || v.val === null)
-        ) {
+        if (q && v) {
+            if ( q.val !== null && v.val !== null) {
+                if (q.val <= v.val) {
+                    curr.next = new ListNode(q.val);
+                    curr = curr.next;
+                    q = q.next;        
+                } else {
+                    curr.next = new ListNode(v.val);
+                    curr = curr.next;
+                    v = v.next;
+                }
+            } else if (q.val !== null) {
+                curr.next = new ListNode(q.val);
+                curr = curr.next;
+                q = q.next;
+            } else {
+                curr.next = new ListNode(v.val);
+                curr = curr.next;
+                v = v.next;
+            }
+        } else if (q) {
             curr.next = new ListNode(q.val);
             curr = curr.next;
-             q = q.next;
+            q = q.next;
         } else {
             curr.next = new ListNode(v.val);
             curr = curr.next;
-            v = v.next;  
+            v = v.next;
         }    
             
     }
