c3432492aeb64503463d6510fdfb6dbf201c902d
Royalthius <arathurs@gmail.com>
2020-11-16 02:38:00 -0500

    All redundant or other solutions stored in extra.

--- extra/mergeTwoListsIBF.js
+++ extra/mergeTwoListsIBF.js
@@ -12,15 +12,14 @@
  */
 /* This solution beats 96.90% of JavaScript online submissions for
    Merge Two Sorted Lists on LeetCode */
-   var mergeTwoLists = function(l1, l2) {
+var mergeTwoLists = function(l1, l2) {
     let dummyHead = new ListNode(0),
         curr = dummyHead;
     while (l1 || l2) {
         if (l1 && l2 && l1.val !== null && l1.val !== null) {
             if (l1.val > l2.val) {
                 curr.next = l2;
-                l2 = l2.next;
-                
+                l2 = l2.next;      
             } else {
                 curr.next = l1;
                 l1 = l1.next;  
@@ -28,7 +27,6 @@
             curr = curr.next;
             continue;
         } 
-        
         curr.next = l1 || l2;
         break;
     }
